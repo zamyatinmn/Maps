b48ceb1c2c57026ee64c287d50c84f8f6adb9e5f
zamyatinmn <zamyatinmn@gmail.com>
2022-04-24 19:13:13 +0300

    Sketch

--- app/src/main/java/com/zamyatinmn/maps/MainActivity.kt
+++ app/src/main/java/com/zamyatinmn/maps/MainActivity.kt
@@ -10,6 +10,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.zamyatinmn.maps.ui.theme.MapsTheme
 
 class MainActivity: ComponentActivity() {
@@ -29,7 +34,20 @@ class MainActivity: ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            position = singapore,
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
+    }
 }
 
 @Preview(showBackground = true)

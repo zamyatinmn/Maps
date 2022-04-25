286d07c6472523c8966a0919c65a855579e6822e
Maxim Zamyatin <mzamyatin@abycus.com>
2022-04-25 10:11:48 +0500

    First step

--- app/src/main/java/com/zamyatinmn/maps/MainActivity.kt
+++ app/src/main/java/com/zamyatinmn/maps/MainActivity.kt
@@ -3,50 +3,79 @@ package com.zamyatinmn.maps
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.*
 import com.zamyatinmn.maps.ui.theme.MapsTheme
 
-class MainActivity: ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MapsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                Map(mutableListOf())
             }
         }
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun Greeting(name: String) {
-    val singapore = LatLng(1.35, 103.87)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
-    }
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
-    ) {
-        Marker(
-            position = singapore,
-            title = "Singapore",
-            snippet = "Marker in Singapore"
-        )
+fun Map(markerss: MutableList<LatLng>) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        var isMapLoaded by remember { mutableStateOf(false) }
+        val markers = remember { mutableStateOf(markerss) }
+        var singapore by remember {
+            mutableStateOf(LatLng(1.35, 103.87))
+        }
+        markers.value.add(singapore)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        }
+        val mapProperties by remember { mutableStateOf(MapProperties(isMyLocationEnabled = true)) }
+        val uiSettings by remember { mutableStateOf(MapUiSettings(myLocationButtonEnabled = true)) }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
+            properties = mapProperties,
+            uiSettings = uiSettings,
+            onMapLoaded = {
+                isMapLoaded = true
+            },
+            onMapLongClick = {
+                singapore = it
+            }
+        ) {
+            markers.value.forEach { Marker(position = it) }
+        }
+        if (!isMapLoaded) {
+            AnimatedVisibility(
+                modifier = Modifier
+                    .matchParentSize(),
+                visible = !isMapLoaded,
+                enter = EnterTransition.None,
+                exit = fadeOut()
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .background(MaterialTheme.colors.background)
+                        .wrapContentSize()
+                )
+            }
+        }
     }
 }
 
@@ -54,6 +83,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MapsTheme {
-        Greeting("Android")
+        Map(mutableListOf())
     }
 }
